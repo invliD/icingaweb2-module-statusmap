5645af3841e5ef0e633e40fca1a43ba545b0c8f2
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2017-08-12 01:05:43 +0200

    replaced own ido model with a customvar

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,55 +5,61 @@ use Icinga\Module\Monitoring\Controller as MonitoringController;
 use Icinga\Module\Monitoring\Object\HostList;
 use Icinga\Web\Url;
 
-class StatusMap_IndexController extends MonitoringController {
+class StatusMap_IndexController extends MonitoringController
+{
 
-	protected $hostList;
+    protected $hostList;
 
-	public function init() {
-		$this->hostList = new HostList($this->backend);
-		$this->hostList->addFilter(Filter::matchAll());
+    public function init()
+    {
+        $this->hostList = new HostList($this->backend);
+        $this->hostList->addFilter(Filter::matchAll());
 
-		$this->getTabs()->add(
-			'show',
-			array(
-				'title' => 'Show Status Map',
-				'label' => 'Status Map',
-				'url' => Url::fromRequest(),
-			)
-		)->activate('show');
-	}
+        $this->getTabs()->add(
+            'show',
+            array(
+                'title' => 'Show Status Map',
+                'label' => 'Status Map',
+                'url' => Url::fromRequest(),
+            )
+        )->activate('show');
+    }
 
-	public function indexAction() {
-		$this->hostList->setColumns(array(
-			'host_name',
-			'host_display_name',
-			'host_state',
-		));
+    public function indexAction()
+    {
+        $this->hostList->setColumns(array(
+            'host_name',
+            'host_display_name',
+            'host_state',
+        ));
 
-		$hosts = $this->hostList->fetch();
+        $hosts = $this->hostList->fetch();
 
-		$dependencies = $this->backend->select()
-			->from('hostdependency', array('host_name', 'dependent_host_name'))
-			->fetchAll();
+        $dependencies= $this->backend
+            ->select()
+            ->from('customvar', array(
+                'host_name',
+                'varvalue'))
+            ->where('varname', 'parent_host_name')->fetchAll();
 
-		$data_dep = array();
-		foreach ($dependencies as $dependency) {
-			$data_dep[] = array(
-				'parent' => $dependency->host_name,
-				'child' => $dependency->dependent_host_name,
-			);
-		}
-		$this->view->data_dependencies = json_encode($data_dep);
+        $data_dep = array();
+        foreach ($dependencies as $dependency) {
+            $data_dep[] = array(
+                'parent' => $dependency->host_name,
+                'child' => $dependency->varvalue,
+            );
+        }
+        $this->view->data_dependencies = json_encode($data_dep);
 
-		$data_hosts = array();
-		foreach ($hosts as $host) {
-			$data_hosts[] = array(
-				'host_name' => $host->host_name,
-				'display_name' => $host->host_display_name,
-				'state' => (int) $host->host_state,
-			);
-		}
-		$this->view->data_hosts = json_encode($data_hosts);
-	}
+        $data_hosts = array();
+        foreach ($hosts as $host) {
+            $data_hosts[] = array(
+                'host_name' => $host->host_name,
+                'display_name' => $host->host_display_name,
+                'state' => (int)$host->host_state,
+            );
+        }
+        $this->view->data_hosts = json_encode($data_hosts);
+    }
 
 }
